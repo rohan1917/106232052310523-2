9df8966803b024f9f615d4a72306c38304f62a03
Rohan Kinnal <64979784+rohan1917@users.noreply.github.com>
2023-07-20 15:01:36 +0000

    intojs

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-console.log("Hello World");
+//console.log("Hello World");
+// const state = {
+//     taskList: [
+//         // const taskList = [
+//         {
+//             image: "",
+//             title: "",
+//             type: "",
+//             description: "",
+//         },
+//          {
+//             image: "",
+//             title: "",
+//             type: "",
+//             description: "",
+//         },
+//          {
+//             image: "",
+//             title: "",
+//             type: "",
+//             description: "",
+//         },
+//          {
+//             image: "",
+//             title: "",
+//             type: "",
+//             description: "",
+//         },
+//          {
+//             image: "",
+//             title: "",
+//             type: "",
+//             description: "",
+//         },
+//     ]
+// }
+
+
+const state = {
+    taskList: [],
+};
+
+// DOM 
+const taskContents = document.querySelector(".task__contents");
+const taskModal = document.querySelector(".task__modal__body");
+
+// console.log(taskContents);
+// console.log(taskModal);
+
+
+const htmlTaskContent = ({id, title, description, type, url}) => {
+
+};
