43644343b1e6594e297bbfa27ac4d7f41ef5a061
Rohan Kinnal <64979784+rohan1917@users.noreply.github.com>
2023-07-25 15:01:35 +0000

    crud

--- index.js
+++ index.js
@@ -53,12 +53,15 @@ const htmlTaskContent = ({id, title, description, type, url}) => `
         <div class='card shadow task__card'>
             <div class='card-header d-flex gap-2 justify-content-end task__card__header'>
                 <button type='button' class='btn btn-outline-info mr-2' name=${id}><i class='fas fa-pencil-alt'></i></button>
-                <button type='button' class='btn btn-outline-danger mr-2' name=${id}><i class='fas fa-trash-alt'></i></button>
+                <button type='button' class='btn btn-outline-danger mr-2' name=${id} onclick="deleteTask.apply(this, arguments)">
+                <i class='fas fa-trash-alt'></i></button>
             </div>
             <div class='card-body'>
                 ${
-                    url && 
+                    url ?
                      `<img src=${url} alt='card image class='card-img-top md-3 rounded-md' />`
+                     :
+                     `<img src="https://tse3.mm.bing.net/th?id=OIP.FjLkalx51D8xJcpixUGJywHaE8&pid=Api&P=0&h=180" alt='card image class='card-img-top md-3 rounded-md' />`
                 }
                 <h4 class='card-title'>${title}</h4>
                 <p class='card-text text-muted'>${description}</p>
@@ -68,7 +71,7 @@ const htmlTaskContent = ({id, title, description, type, url}) => `
             </div>
            <div class='card-footer'>
                 <button type='button' class='btn btn-outline-primary float-end' data-bs-toggle='modal' 
-                data-bs-target='#showTask'>Open Task</button>
+                data-bs-target='#showTask' id=${id} onclick='openTask.apply(this, arguments)'>Open Task</button>
             </div>
         </div>
     </div>
@@ -81,8 +84,10 @@ const htmlModalContent = ({id, title, description, url}) => {
     return `
     <div id=${id}>
         ${
-                    url && 
+                    url ?
                      `<img src=${url} alt='card image class='img-fluid rounded place__holder__image mb-3'  />`
+                     :
+                       `<img src="https://tse3.mm.bing.net/th?id=OIP.FjLkalx51D8xJcpixUGJywHaE8&pid=Api&P=0&h=180" alt='card image class='card-img-top md-3 rounded-md' />`
                 }
         <strong class='text-sm text-muted'>Created on ${date.toDateString()}</strong>    
          <h4 class='my-2'>${title}</h4>  
@@ -143,3 +148,45 @@ const handleSubmit = () =>{
 
 // Default img
 // https://tse3.mm.bing.net/th?id=OIP.FjLkalx51D8xJcpixUGJywHaE8&pid=Api&P=0&h=180
+
+
+
+// Opening modal from the cards
+const openTask = (e) => {
+    if(!e) e = window.event;
+    
+    //console.log("event" , e);
+
+    const getTask = state.taskList.find(({id}) => id === e.target.id);
+     taskModal.innerHTML= htmlModalContent(getTask)
+}
+
+
+
+// CRUD => Delete Operation
+const deleteTask = (e) => {
+    if(!e) e = window.event;
+    const targetID = e.target.getAttribute("name");
+    // console.log(e.target);
+    const type = e.target.tagName;
+    console.log(type);
+    const removeTask = state.taskList.filter(({id}) => id !== targetID);
+    // console.log(removeTask);
+    state.taskList = removeTask;
+    
+    console.log("updated arr", state.taskList);
+
+    updateLocalStorage();
+// I was 
+    if(type === "BUTTON"){
+        // console.log(e.target.parentNode.parentNode.parentNode);
+        return e.target.parentNode.parentNode.parentNode.parentNode.removeChild(
+            e.target.parentNode.parentNode.parentNode
+        );
+    }else if(type==="I"){
+    return e.target.parentNode.parentNode.parentNode.parentNode.parentNode.removeChild(
+            e.target.parentNode.parentNode.parentNode.parentNode
+        );
+        }
+//  updateLocalStorage();
+}
