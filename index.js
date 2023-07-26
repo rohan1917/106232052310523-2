d6d1c8f7582f57b010311a669e1f6e5a3b26c05e
Rohan Kinnal <64979784+rohan1917@users.noreply.github.com>
2023-07-26 15:38:46 +0000

    done except search

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ const htmlTaskContent = ({id, title, description, type, url}) => `
     <div class='col-md-6 col-lg-4 mt-3'  id=${id} key=${id}>
         <div class='card shadow task__card'>
             <div class='card-header d-flex gap-2 justify-content-end task__card__header'>
-                <button type='button' class='btn btn-outline-info mr-2' name=${id}><i class='fas fa-pencil-alt'></i></button>
+                <button type='button' class='btn btn-outline-info mr-2' name=${id} onclick="editTask.apply(this, arguments)"><i class='fas fa-pencil-alt'></i></button>
                 <button type='button' class='btn btn-outline-danger mr-2' name=${id} onclick="deleteTask.apply(this, arguments)">
                 <i class='fas fa-trash-alt'></i></button>
             </div>
@@ -177,7 +177,7 @@ const deleteTask = (e) => {
     console.log("updated arr", state.taskList);
 
     updateLocalStorage();
-// I was 
+// I tag was not working fine
     if(type === "BUTTON"){
         // console.log(e.target.parentNode.parentNode.parentNode);
         return e.target.parentNode.parentNode.parentNode.parentNode.removeChild(
@@ -189,4 +189,96 @@ const deleteTask = (e) => {
         );
         }
 //  updateLocalStorage();
+}
+
+
+const editTask = (e) => {
+    if(!e) e = window.event;
+
+    const targetID = e.target.id;
+    const type = e.target.tagName;
+
+    let parentNode;
+    let taskTitle;
+    let taskDescription;
+    let taskType;
+    let submitButton;
+
+    if(type === "BUTTON"){
+        parentNode = e.target.parentNode.parentNode;
+        // console.log(parentNode);
+    }else{
+        parentNode = e.target.parentNode.parentNode.parentNode;
+    }
+
+    // Uncomment following 2 lines to know abt childNodes
+    // taskTitle = parentNode.childNodes;
+    // console.log(taskTitle);
+    
+    taskTitle = parentNode.childNodes[3].childNodes[3];
+    taskDescription = parentNode.childNodes[3].childNodes[5];
+    taskType = parentNode.childNodes[3].childNodes[7].childNodes[1];
+    // console.log(taskTitle, taskDescription, taskType);
+    submitButton = parentNode.childNodes[5].childNodes[1];
+    // console.log(submitButton);
+
+    taskTitle.setAttribute("contenteditable", "true");
+    taskDescription.setAttribute("contenteditable", "true");
+    taskType.setAttribute("contenteditable", "true");
+
+    submitButton.setAttribute('onclick', "saveEdit.apply(this, arguments)");
+    submitButton.removeAttribute("data-bs-toggle");
+    submitButton.removeAttribute("data-bs-target");
+    submitButton.innerHTML = "Save Changes";
+
+};
+
+// ... spread opeartor
+const saveEdit = (e) => {
+    if(!e) e=window.event;
+
+    const targetID = e.target.id;
+    const parentNode = e.target.parentNode.parentNode;
+    // console.log(parentNode);
+
+   const taskTitle = parentNode.childNodes[3].childNodes[3];
+   const taskDescription =  parentNode.childNodes[3].childNodes[5];
+   const taskType = parentNode.childNodes[3].childNodes[7].childNodes[1];
+   const submitButton = parentNode.childNodes[5].childNodes[1];
+
+    const updateData = {
+        taskTitle: taskTitle.innerHTML,
+        taskDescription: taskDescription.innerHTML,
+        taskType: taskType.innerHTML,
+    };
+    // console.log(updateData);
+
+    // updating the latest data on our local array
+    let stateCopy = state.taskList;
+
+   
+    stateCopy = stateCopy.map((task)=> 
+    task.id === targetID
+    ? {
+        id: task.id,
+        title: updateData.taskTitle,
+        description: updateData.taskDescription,
+        type: updateData.taskType,
+    }
+    : task
+     );
+     state.taskList = stateCopy;
+     console.log(state.taskList);
+
+     updateLocalStorage();
+    taskTitle.setAttribute("contenteditable", "false");
+    taskDescription.setAttribute("contenteditable", "false");
+    taskType.setAttribute("contenteditable", "false");
+
+
+
+    submitButton.setAttribute('onclick', "openTask.apply(this, arguments)");
+    submitButton.setAttribute("data-bs-toggle", "modal");
+    submitButton.setAttribute("data-bs-target", "#showTask");
+    submitButton.innerHTML = "Open Task";
 }
